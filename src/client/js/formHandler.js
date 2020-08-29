62a740453806fba266f6333ed8302134d7316420
Lena Wagner <lena_wagner@outlook.de>
2020-08-29 23:10:53 +0200

    loaders, plugins, API CALL

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -3,13 +3,34 @@ function handleSubmit(event) {
 
     // check what text was put into the form field
     let formText = document.getElementById('name').value
-    checkForName(formText)
+    Client.checkForName(formText)
 
     console.log("::: Form Submitted :::")
-    fetch('http://localhost:8080/test')
-    .then(res => res.json())
-    .then(function(res) {
-        document.getElementById('results').innerHTML = res.message
+
+    const postData = async (url='', data={} =>
+        const response = await fetch(url, {
+            method: 'POST',
+            credentials: 'same-origin',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data),
+        });
+        console.log(response);
+        try {
+            const newData = await response.json();
+            console.log(newData);
+            // appropriately handle the error
+        } catch (error) {
+            console.log('Unable to POST data', error);
+        }
+    postData('http://localhost:8081/add', options);
+    .then(res => {
+     return res.json()
+    })
+    .then(function(data) {
+        document.getElementById('subjectivity').innerHTML = data.subjectivity;
+        document.getElementById('agreement').innerHTML = data.agreement;
     })
 }
 
--- src/server/index.js
+++ src/server/index.js
@@ -1,3 +1,10 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
+// Setup empty JS object to act as endpoint for all routes
+const projectData = {};
+
+
 var path = require('path')
 const express = require('express')
 const mockAPIResponse = require('./mockAPI.js')
@@ -13,8 +20,72 @@ app.get('/', function (req, res) {
 })
 
 // designates what port the app will listen to for incoming requests
-app.listen(8080, function () {
-    console.log('Example app listening on port 8080!')
+app.listen(8081, function () {
+    console.log('Example app listening on port 8081!')
+})
+
+
+//API
+
+
+const application_key = process.env.API_KEY
+// check what text was put into the form field
+const formText = document.getElementById('name').value
+
+var https = require('follow-redirects').https;
+var fs = require('fs');
+
+var options = {
+  'method': 'POST',
+  'hostname': 'api.meaningcloud.com',
+  'path': `/sentiment-2.1?key=${application_key}&lang=auto&txt=${formText}`
+  'headers': {
+  },
+  'maxRedirects': 20
+};
+
+var req = https.request(options, function (res) {
+  var chunks = [];
+
+  res.on("data", function (chunk) {
+    chunks.push(chunk);
+  });
+
+  res.on("end", function (chunk) {
+    var body = Buffer.concat(chunks);
+    console.log(body.toString());
+  });
+
+  res.on("error", function (error) {
+    console.error(error);
+  });
+});
+
+req.end();
+
+
+/*let options = {
+    method: 'POST',
+    hostname: 'api.meaningcloud.com',
+    path: `/sentiment-2.1?key=${application_key}&lang=auto&txt=${formText}`,
+    headers: {
+          'Content-Type': 'application/json',
+    },
+    maxRedirects: 20
+};*/
+
+/*app.post('/add', addEntry);
+
+function addEntry(request, response){
+    //add entries to projectData object
+    projectData['name'] = request.body.name;
+    response.send(projectData);
+    console.log('Post has been received');
+};*/
+
+
+app.get('/add', function(req,res){
+    res.send(options)
 })
 
 app.get('/test', function (req, res) {
