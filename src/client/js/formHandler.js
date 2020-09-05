e894fbfebe7e71f9d5f3648c8c341d031550c4f4
esnue <lena_wagner@outlook.de>
2020-09-05 19:32:57 +0200

    fixed prod API Call, need to fix dev API call

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,30 +1,36 @@
 function handleSubmit(event) {
-    event.preventDefault()
+  event.preventDefault()
 
-    // check what text was put into the form field
-    let formText = document.getElementById('name').value
-    Client.checkForName(formText)
+  // check what text was put into the form field
+  let formText = document.getElementById('name').value
+  Client.checkForName(formText)
 
-    console.log("::: Form Submitted :::")
+  console.log("::: Form Submitted :::")
 
+  if (formText == "") {
+    alert("Please fill out the form");
+    return false;
+  }
+  else {
     let settings = {
-        "url": "http://localhost:8080/APIresponse",
-        "method": "POST",
-        "timeout": 0,
-        "data": {
-            txt: encodeURI(formText),
-            model: "general"
-        },
-        "dataType": "json"
-      };
-    
-      $.ajax(settings).done(function (response) {
-        console.log(response);
-        let json = $.parseJSON(response);
-        document.getElementById('subjectivity').innerHTML = "Subjectivity: " + json.subjectivity;
-        document.getElementById('agreement').innerHTML = "Agreement: " + json.agreement;
-        document.getElementById('irony').innerHTML = "Variant: " + json.irony;
-      });
+      "url": "/APIresponse",
+      "method": "POST",
+      "timeout": 0,
+      "data": {
+        txt: encodeURI(formText),
+        model: "general"
+      },
+      "dataType": "json"
+    };
+
+    $.ajax(settings).done(function (response) {
+      console.log(response);
+      let json = $.parseJSON(response);
+      document.getElementById('subjectivity').innerHTML = "Subjectivity: " + json.subjectivity;
+      document.getElementById('agreement').innerHTML = "Agreement: " + json.agreement;
+      document.getElementById('irony').innerHTML = "Variant: " + json.irony;
+    });
+  }
 }
 
 export { handleSubmit }
--- webpack.prod.js
+++ webpack.prod.js
@@ -30,7 +30,7 @@ module.exports = {
             template: "./src/client/views/index.html",
             filename: "./index.html",
         }),
-        new MiniCssExtractPlugin({ filename: "[name].css" })
+        new MiniCssExtractPlugin({ filename: "[name].css" }),
     ],
     optimization: {
         minimizer: [new TerserPlugin({}), new OptimizeCSSAssetsPlugin({})],
