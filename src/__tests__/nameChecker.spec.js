2d6e0e60874bfa10d38397ff29524c61483e0b67
esnue <lena_wagner@outlook.de>
2020-09-08 21:55:50 +0200

    debugged jest tests

--- src/__tests__/nameChecker.spec.js
+++ src/__tests__/nameChecker.spec.js
@@ -1,10 +1,8 @@
-import { checkForName } from '.src\client\js\nameChecker.js';
+import { checkForName } from '../client/js/nameChecker'
 
 describe("search function", () => {
     test('it should check if names are included', () => {
         expect(checkForName).toBeDefined();
-        expect(checkForName).toBe(true);
+        //expect(checkForName).toBe(true);
     });
 });
-
-
---
+++ src/__tests__/validURL.spec.js
@@ -0,0 +1,11 @@
+import { validURL } from '../client/js/validURL'
+
+describe("URL validity testing", () => {
+    let testURL = "https://www.nytimes.com/2020/09/07/us/politics/trump-election-campaign-fundraising.html";
+    //let testURL = document.getElementById('url');
+    test("TRUE", () => {
+        const result = validURL(testURL);
+        expect(result).toBeDefined();
+        expect(result).toBe(true);
+    });
+});
