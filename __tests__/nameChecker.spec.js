1567803d47c1cd104a3d9f2beba37b6b2c7a9893
esnue <lena_wagner@outlook.de>
2020-09-08 14:13:02 +0200

    minor tweak

--- __tests__/nameChecker.spec.js
+++ __tests__/nameChecker.spec.js
@@ -2,6 +2,7 @@ import { checkForName } from 'src\client\js\nameChecker.js';
 
 describe("search function", () => {
     test('it should check if names are included', () => {
+        expect(checkForName).toBeDefined();
         expect(checkForName).toBe(true);
     });
 });
