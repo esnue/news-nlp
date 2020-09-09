c0488798b143c531138010558437ea9bdaa7bca0
esnue <lena_wagner@outlook.de>
2020-09-09 11:43:56 +0200

    optimized index test

--- src/__tests__/index.spec.js
+++ src/__tests__/index.spec.js
@@ -1,5 +1,7 @@
 import { sum } from '..src\server\index.js';
 
-test('adds 2 + 3 to equal 5', () => {
-    expect(sum(2, 3)).toBe(5);
-  });
+describe('test server/index.js', () => {
+    test('adds 2 + 3 to equal 5', () => {
+        expect(sum(2, 3)).toBe(5);
+    });
+})
