45a5e7fe04727531cc8ebcfe76f7731dfb550919
esnue <lena_wagner@outlook.de>
2020-09-09 11:27:06 +0200

    jest test updates

--- src/__mocks__/formHandler.js
+++ src/__mocks__/formHandler.js
@@ -8,10 +8,11 @@ function handleSubmit(event) {
     console.log("::: Form Submitted :::")*/
     fetch('http://localhost:8081/test')
     .then(res => res.json())
-    .then(function(res) {
+    return res.json()
+    /*.then(function(res) {
         console.log(res.time);
         return res.time;
-    })
+    })*/
 }
 
 export { handleSubmit }
--- src/__tests__/formHandler.spec.js
+++ src/__tests__/formHandler.spec.js
@@ -3,7 +3,7 @@ import { handleSubmit } from '..\__mocks__\formHandler.js'
 //If mockAPI call from server/index.js performs correctly, mockAPI respond json.time should be now
 describe('Test mockAPI', () => {
     test('It should respond with time:now', async () => {
-      //const response = await request(app).get('/test');
-      expect(res.time).toBe('now');
+      const response = await request(app).get('/test');
+      expect(response.json).toBe('time: now');
     });
 });
